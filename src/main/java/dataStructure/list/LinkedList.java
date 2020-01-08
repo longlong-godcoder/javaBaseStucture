56f4750372a1b7a2192d3a236d9a5ee757d934c8
longlong <743096549@qq.com>
2020-01-08 19:16:44 +0800

    dell commit

--- src/main/java/dataStructure/list/LinkedList.java
+++ src/main/java/dataStructure/list/LinkedList.java
@@ -19,7 +19,7 @@ public class LinkedList<E> extends AbstractList<E>{
 
     @Override
     public void add(int index, E element) {
-
+        rangeCheckForAdd(index);
         if (index == 0){
             first = new Node<>(element, first);
         }else {
@@ -45,6 +45,7 @@ public class LinkedList<E> extends AbstractList<E>{
 
     @Override
     public E remove(int index) {
+        rangeCheck(index);
         Node<E> node = first;
         if (index == 0){
             first = first.next;
