2d9e1a2eb7ec4d9051044d60a8c98d45e2b67111
longlong <743096549@qq.com>
2020-02-02 15:18:06 +0800

    t480s COMMIT

--- src/main/java/algorithm/sort/shellSort/ShellSort.java
+++ src/main/java/algorithm/sort/shellSort/ShellSort.java
@@ -1,16 +1,51 @@
 package algorithm.sort.shellSort;
 
 import algorithm.sort.Sort;
-import dataStructure.list.LinkedList;
-import dataStructure.list.List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
+/**
+ * 希尔排序
+ * 主要思想：根据步长序列，进行分步分组排序。
+ * 比如步长序列是{2，4，8}，就先将数组分成8组分别排序，以此类推
+ * @param <T>
+ */
 public class ShellSort<T extends Comparable<T>> extends Sort<T> {
 
     @Override
     protected void sort() {
-
+        //这里采用sedgewick步长序列，效率更高
+        List<Integer> stepSequence = sedgewickStepSequence();
+        for (Integer step :
+                stepSequence) {
+            sort(step);
+        }
     }
 
+    private void sort(int step){
+        //根据step步长，分成多列进行单独排序
+        for (int col = 0; col < step; col++) {
+            //遍历同一列的每个元素, 这里局部采用插入排序
+            for (int begin = col + step; begin < array.length; begin+=step) {
+                int current = begin;
+                while (current > col && cmp(current, current - step) < 0){
+                    swap(current, current - step);
+                    current -= step;
+                }
+            }
+        }
+    }
+    //希尔本人的步长序列，时间复杂度最差是n2次方
+    private List<Integer> shellStepSequence(){
+        ArrayList<Integer> stepSequence = new ArrayList<>();
+        int step = array.length;
+        while ((step >>= 1) > 0){
+            stepSequence.add(step);
+        }
+        return stepSequence;
+    }
+    //1986年 Robert sedgewick提出的步长序列，最坏时间复杂度n(4/3)次方
     private List<Integer> sedgewickStepSequence(){
         LinkedList<Integer> stepSequence = new LinkedList<>();
         int k = 0,step = 0;
